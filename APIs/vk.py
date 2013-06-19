516e247ac3f0ebbe9b1a73f568e726a9368b9087
Infernion <sergiykhalimon@gmail.com>
2013-06-19 21:00:28 +0300

    receive name, country, city and photo from user

--- APIs/vk.py
+++ APIs/vk.py
@@ -7,7 +7,7 @@ import urllib
 from google.appengine.api import urlfetch
 
 import handler
-import model
+from model import User
 
 
 KEY = '3720247'
@@ -24,7 +24,6 @@ code: Get var with auth code from vk.
 Returns:
 Auths user and redirects him to home page.
 """
-
   def get(self):
     code = self.request.get('code')
     payload = urllib.urlencode({
@@ -36,19 +35,27 @@ Auths user and redirects him to home page.
       url='https://oauth.vk.com/access_token?%s' % payload,
       method=urlfetch.GET).content
     token_json = json.loads(token)
-
     token = token_json['access_token']
     uid = token_json['user_id']
+    user_info_json = urlfetch.fetch(
+        url='https://api.vk.com/method/users.get?uid=%s&fields=uid,first_name,last_name,city,country,photo_rec&access_token=%s' % (uid, token),
+        method=urlfetch.POST).content
+    user_info = json.loads(user_info_json)
+    user_info = user_info['response'][0]
 
-    self.response.out.write(uid)
+    name, countryid, cityid, photo_url = ('%s %s' % (user_info['first_name'], user_info['last_name']),
+                                   user_info['country'],
+                                   user_info['city'],
+                                   user_info['photo_rec'])
 
-    #user_info = urlfetch.fetch(url='https://api.vk.com/method/users.get?uids=%s&fields=uid,first_name,last_name,city,country,photo_rec&access_token=%s' % (uid, token),
-    # method=urlfetch.POST).content
-    
-    #user_info = json.loads(user_info)
-    
-    #city = urlfetch.fetch(url='https://api.vk.com/method/getCities?cids=665&access_token=%s' % token, method=urlfetch.POST).content
-    
-    #country = urlfetch.fetch(url='https://api.vk.com/method/getCountries?cids=1,2,3,4,5&access_token=%s' % token, method=urlfetch.POST).content
- 
-    #return self.redirect('/')
+    country_json = urlfetch.fetch(
+        url='https://api.vk.com/method/places.getCountryById?cids=%s&access_token=%s' % (countryid, token),
+        method=urlfetch.POST).content
+    country = country_json['response'][0]['name']
+
+    city_json = urlfetch.fetch(
+        url='https://api.vk.com/method/places.getCityById?cids=%s&access_token=%s' % (cityid, token),
+        method=urlfetch.POST).content
+    city = city_json['response'][0]['name']
+
+    self.response.out.write(name, country, city, photo_url)
