4be201a80a9a820ed696a47c9e906022f726c82d
Infernion <sergiykhalimon@gmail.com>
2013-06-19 14:22:45 +0300

    some stuffs

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# -*- coding: utf-8 -*-
+
+import logging
 import webapp2
+import handler
+from APIs.vk import KEY, SECRET
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+class Home(handler.Base):
+  def get(self):
+    self.render('home.html', key=KEY, secret=SECRET)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+class Logout(handler.Base):
+  def get(self):
+    self.session['user_id'] = None
+    return self.redirect('/')
+
+
+config = {}
+config['webapp2_extras.sessions'] = {
+  'secret_key': 'MY SUPER SECRET COOKIE KEY FOR VK APPLICATION!!!11',
+  'session_max_age': None,
+  'cookie_args': {
+    'max_age': 31556926,
+    'domain': None,
+    'path': '/',
+    'secure': None,
+    'httponly': True,
+  },
+}
+
+logging.getLogger().setLevel(logging.DEBUG)
+application = webapp2.WSGIApplication([
+  (r'/', Home),
+  (r'/auth/vk', 'vk.Auth'),
+  (r'/logout', Logout),
+
+], debug=True, config=config)
