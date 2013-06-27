285b26fa1767224afec7b059a670a9b7c01f39e5
Infernion <sergiykhalimon@gmail.com>
2013-06-27 07:06:35 +0300

    Update api_keys.py

--- APIs/api_keys.py
+++ APIs/api_keys.py
@@ -1,8 +1,8 @@
 __author__ = 'infernion'
 
-GMAP_API_KEY = "AIzaSyCvlqGS5GcOZ2uLz7LsSdSSL6dFSMSnk8w"
+GMAP_API_KEY = ""
 
-VK_APP_KEY = '3733644'
-VK_APP_SECRET = 'ZdbfMIFFl5JWd8FyfqMv'
+VK_APP_KEY = ''
+VK_APP_SECRET = ''
 VK_AUTH_REDIRECT_URI = r'http://dev.friends-on-map.appspot.com/auth/vk'
 
