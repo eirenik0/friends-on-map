67dead096c127d918991a4cc2bb1e423089e9c42
infernion <sergiykhalimon@gmail.com>
2013-06-29 13:27:16 +0300

    del some rudiment method, and add better solution

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+__author__ = 'infernion'
 
 from google.appengine.api import memcache
 from APIs.api_keys import GMAP_API_KEY, VK_APP_KEY, VK_APP_SECRET, VK_AUTH_REDIRECT_URI
+from APIs.geocode import Geocode
 import logging
 import webapp2
 import handler
@@ -14,63 +16,44 @@ class Home(handler.Base):
     def get(self):
         self.render('home.html', vk_key=VK_APP_KEY, vk_secret=VK_APP_SECRET, vk_redirect_url=VK_AUTH_REDIRECT_URI)
 
+
 class Map(handler.Base):
     def get(self):
         self.request.headers['Content-Type'] = 'text/plain'
-        self.uid = self.request.cookies.get('uid')  # get uid(key) from cookie
-        user = model.User.get_by_id(self.uid)
-        address, coord = self.location(user)
-        self.render('map.html', user=user, address=address, location=coord, gmap_key=GMAP_API_KEY)
-
-    def friends_to_json(self, friends):
-        friends_no_json = []
-        for friend in friends:
-          #  if len(friends_no_json) < 500:
-                try:
-                    friends_no_json.append({'lat': friend['location'][0],
-                                            'lng': friend['location'][1],
-                                            'name': friend['name'],
-                                            'address': friend['address'],
-                                            'url': "https://vk.com/%s" % friend['uid'],
-                                            'photo': friend['photo']})
-                except:
-                    pass
-        friends_json = json.dumps(friends_no_json)
-        return friends_json
-
-    def location(self, user):
-        """
-        :param user: user id(uid)
-        :return: address from ndb, coordinates from IP
-        """
+        uid = self.request.cookies.get('uid')  # get uid(key) from cookie
+        user = model.User.get_by_id(uid)
         address = '%s, %s' % (user.country, user.city)
-        coords = self.get_coords(self.request.remote_addr)  # receive location from id
-        if coords == None:
-            coords = "36.315125,-27.802738"
-            return address, coords
-        return address, coords
+        geo = Geocode().get(address)
+        coords = "%s, %s" % (geo[0], geo[1])
+        print coords
+        self.render('map.html', user=user, address=address, location=coords, gmap_key=GMAP_API_KEY)
 
 
 class Json(Map):
     def get(self):
         self.request.headers['Content-Type'] = 'text/plain'
-        self.uid = self.request.cookies.get('uid')  # get uid(key) from cookie
-        friends = memcache.get('%s friends' % self.uid)
+        uid = self.request.cookies.get('uid')
+        friends = memcache.get('%s friends' % uid)
         friends_json = self.friends_to_json(friends)
         self.write(friends_json)
 
+    def friends_to_json(self, friends):
+        friends_no_json = []
+        for friend in friends:
+            try:
+                friends_no_json.append({'lat': friend['location'][0],
+                                        'lng': friend['location'][1],
+                                        'name': friend['name'],
+                                        'address': friend['address'],
+                                        'url': "https://vk.com/%s" % friend['uid'],
+                                        'photo': friend['photo']})
+            except:
+                pass
+        friends_json = json.dumps(friends_no_json)
+        return friends_json
 
-class StaticMap(Map):
-    def get(self):
-        #uid = self.get_cookie('uid')
-        #user = model.User.get_by_id(uid)
-        #address, coord = self.location(user)
-        self.render('map_static.html', photo='http://cs302810.vk.me/v302810263/40d5/NhHItwnqQxc.jpg',
-                    address=u'Ukraine,Харьков')
 
-
-config = {}
-config['webapp2_extras.sessions'] = {
+config = {'webapp2_extras.sessions': {
     'secret_key': 'LKsdt4223o5khsdt9',
     'session_max_age': None,
     'cookie_args': {
@@ -80,15 +63,14 @@ config['webapp2_extras.sessions'] = {
         'secure': None,
         'httponly': True,
     },
-}
+}}
 
 logging.getLogger().setLevel(logging.DEBUG)
 application = webapp2.WSGIApplication([
     (r'/', Home),
     (r'/auth/vk', 'APIs.vk.Auth'),
     (r'/map', Map),
-    (r'/map-static', StaticMap),
+    #(r'/map-static', StaticMap),
     (r'/json', Json),
-
 
 ], debug=True, config=config)
